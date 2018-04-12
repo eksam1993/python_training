13a7e6cabadf6fbb6862f9e7c0d2c8b489cb6a17
eksam1993 <ek.samoylova@mail.ru>
2018-04-12 15:33:32 +0300

    Добавлен Object

---
+++ conacts.py
@@ -0,0 +1,9 @@
+class Contacts:
+
+    def __init__(self, firstname, lastname, nickname, title, company, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.email = email
