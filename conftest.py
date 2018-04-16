2195b1d5d16f6a4dd820b8bff94609c1b62058c0
eksam1993 <ek.samoylova@mail.ru>
2018-04-16 17:06:44 +0300

    Удалена лишняя фикстура application

--- conftest.py
+++ conftest.py
@@ -1,17 +1,9 @@
 import pytest
 from fixture.application import Application
-from fixture.application2 import Application2
 
 
 @pytest.fixture(scope = "session")
 def app(request):
     fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
-
-
-@pytest.fixture(scope = "session")
-def app(request):
-    fixture = Application2()
     request.addfinalizer(fixture.destroy)
     return fixture
