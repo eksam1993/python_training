7e2599c802d052c2c8c37df6c5312ecb5cdd1489
eksam1993 <ek.samoylova@mail.ru>
2018-04-16 16:15:07 +0300

    Добавлено удаление группы. Создан conftest.

--- fixture/application2.py
+++ fixture/application2.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
+from fixture.contacts import ContactsHelper
 
 
 class Application2:
@@ -8,34 +9,11 @@ class Application2:
         self.wd = WebDriver(capabilities={"marionette": False})
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
+        self.contacts = ContactsHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost:8080/addressbook/")
-
-    def add_new_contact(self, contacts):
-        wd = self.wd
-        # add new contact
-        wd.find_element_by_link_text("add new").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contacts.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contacts.lastname)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(contacts.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(contacts.title)
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(contacts.company)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(contacts.email)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
     def destroy(self):
         self.wd.quit()
