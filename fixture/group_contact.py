b3f60e21368fc14cbf668b544175237be654110f
eksam1993 <ek.samoylova@mail.ru>
2018-05-30 14:48:42 +0300

    Поправила ошибку в коде

--- fixture/group_contact.py
+++ fixture/group_contact.py
@@ -38,6 +38,7 @@ class GroupHelper:
 
     def select_first_group(self):
         # select first group
+        wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
     def return_to_groups_page(self):
@@ -79,6 +80,7 @@ class GroupHelper:
 
     def select_first_contact(self):
         # select first contact
+        wd = self.app.wd
         wd.find_element_by_name("selected[]").click()
 
     def mod_first_group(self, group):
