2448addc9d06f88b0c6604a4c9c8e99d06075ee2
eksam1993 <ek.samoylova@mail.ru>
2018-05-30 15:53:04 +0300

    error

--- test/test_modif_group.py
+++ test/test_modif_group.py
@@ -1,7 +1,13 @@
 from model.group import Group
 
 
-def test_mod_group(app):
+def test_mod_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.mod_first_group(Group(name="New Name", header="Kokoko 123", footer="Tytyty 456"))
+    app.group.mod_first_group(Group(name="New Name"))
+    app.session.logout()
+
+
+def test_mod_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.mod_first_group(Group(header="New header"))
     app.session.logout()
