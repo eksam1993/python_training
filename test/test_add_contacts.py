d83a20bf4215f8e5d784c3c329f8c8486cbab54c
eksam1993 <ek.samoylova@mail.ru>
2018-05-30 16:48:16 +0300

    Вход и выход перенесены в conftest.py, были упрощены тесты.

--- test/test_add_contacts.py
+++ test/test_add_contacts.py
@@ -3,12 +3,8 @@ from model.contacts import Contacts
 
 
 def test_add_contacts(app):
-        app.session.login(username="admin", password="secret")
         app.group.add_new_contact(Contacts(firstname="Ekaterina", lastname="Samoilova", nickname="eksam", title="123", company="Ek and Co", email="esamoilova@mail.ru"))
-        app.session.logout()
 
 
 def test_add_two_contacts(app):
-        app.session.login(username="admin", password="secret")
         app.group.add_new_contact(Contacts(firstname="Ivan", lastname="Petrov", nickname="ivaN", title="luxry", company="VIPcomp", email="VIOiva123@mail.ru"))
-        app.session.logout()
--- test/test_modif_contact.py
+++ test/test_modif_contact.py
@@ -2,6 +2,4 @@ from model.contacts import Contacts
 
 
 def test_add_contacts(app):
-        app.session.login(username="admin", password="secret")
         app.group.mod_first_contact(Contacts(firstname="Petr", lastname="Ivanov", nickname="petrhulk", title="BIGBOSS", company="Company The Best", email="petrhulk789@vov.vov"))
-        app.session.logout()
